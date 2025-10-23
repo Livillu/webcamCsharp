6adfbebc02514fd2094feb5e7f1bde34b8b79b00
livil.lu <livil.lu@gmail.com>
2025-10-23 08:46:20 +0800

    2025102301

---
+++ Webcam/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Webcam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.butStop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "設定攝影機";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // butStop
+            // 
+            this.butStop.Location = new System.Drawing.Point(96, 47);
+            this.butStop.Name = "butStop";
+            this.butStop.Size = new System.Drawing.Size(112, 23);
+            this.butStop.TabIndex = 2;
+            this.butStop.Text = "Stop Record";
+            this.butStop.UseVisualStyleBackColor = true;
+            this.butStop.Click += new System.EventHandler(this.butStop_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "存錄影檔";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "照相抓圖";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(522, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "關閉";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(90, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(550, 346);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 327);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.butStop);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Vidio";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button butStop;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Webcam/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using AForge.Video.VFW;
+
+namespace Webcam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+  
+        private FilterInfoCollection VideoCaptureDevices=null;//所有攝影機
+        private VideoCaptureDeviceForm captureDevice=null;
+        private Bitmap video;
+        //private AVIWriter AVIwriter = new AVIWriter();
+        private VideoFileWriter FileWriter = new VideoFileWriter();
+        private SaveFileDialog saveAvi;
+        private VideoCaptureDevice FinalVideo = null;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (captureDevice.ShowDialog(this) == DialogResult.OK)
+            {
+                //VideoCaptureDevice videoSource = captureDevice.VideoDevice;
+                FinalVideo = captureDevice.VideoDevice;
+                FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+                FinalVideo.Start();
+            }
+        }
+
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (butStop.Text == "Stop Record")
+            {
+                video = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+                //AVIwriter.Quality = 0;
+                FileWriter.WriteVideoFrame(video);
+                //AVIwriter.AddFrame(video);
+            }
+            else
+            {
+                video = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+            ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            captureDevice = new VideoCaptureDeviceForm();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (FinalVideo == null)
+            { return; }
+            if (FinalVideo.IsRunning)
+            {
+                this.FinalVideo.Stop();
+                FileWriter.Close();
+                //this.AVIwriter.Close();
+            }
+        }
+
+        private void butStop_Click(object sender, EventArgs e)
+        {
+            if (butStop.Text == "Stop Record")
+            {
+                butStop.Text = "Stop";
+                if (FinalVideo == null)
+                { return; }
+                if (FinalVideo.IsRunning)
+                {
+                    //this.FinalVideo.Stop();
+                    FileWriter.Close();
+                    //this.AVIwriter.Close();
+                    pictureBox1.Image = null;
+                }
+            }
+            else
+            {
+                this.FinalVideo.Stop();
+                FileWriter.Close();
+                //this.AVIwriter.Close();
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveAvi = new SaveFileDialog();
+            saveAvi.Filter = "Avi Files (*.avi)|*.avi";
+            if (saveAvi.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int h = captureDevice.VideoDevice.VideoResolution.FrameSize.Height;
+                int w = captureDevice.VideoDevice.VideoResolution.FrameSize.Width;
+                FileWriter.Open(saveAvi.FileName, w, h, 25, VideoCodec.Default, 5000000);
+                FileWriter.WriteVideoFrame(video);
+                //AVIwriter.Open(saveAvi.FileName, w, h);
+                butStop.Text = "Stop Record";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("IMG" + DateTime.Now.ToString("hhmmss") + ".jpg");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                button1.Text = "Stop";
+                Cam.Start();   // WebCam starts capturing images.
+            }
+            else
+            {
+                button1.Text = "Start";
+                Cam.Stop();  // WebCam stops capturing images.
+            }
+        }
+
+        void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (button1.Text == "Stop")
+            {
+                video = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+                //AVIwriter.Quality = 0;
+                FileWriter.WriteVideoFrame(video);
+                //AVIwriter.AddFrame(video);
+            }
+            else
+            {
+                video = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+            //pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDeviceForm();
+            USB_Webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (USB_Webcams.Count > 0)  // The quantity of WebCam must be more than 0.
+            {
+                button1.Enabled = true;
+                Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);
+                Cam.NewFrame += new NewFrameEventHandler(Cam_NewFrame);
+                //Cam.NewFrame += Cam_NewFrame;//Press Tab  to   create
+            }
+            else
+            {
+                button1.Enabled = false;
+                MessageBox.Show("No video input device is connected.");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Cam != null)
+            {
+                if (Cam.IsRunning)  // When Form1 closes itself, WebCam must stop, too.
+                {
+                    Cam.Stop();   // WebCam stops capturing images.
+                }
+            }
+        }*/
+    }
+}
